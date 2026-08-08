0c5bc7992450fa7ad40d50d68cff6a5c189a0236
aviralsingh28 <aviral281201@gamail.com>
2026-08-08 12:34:00 +0530

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["13.234.103.175"],
 };
 
 export default nextConfig;
